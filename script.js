989af14d4943e6a63b66a979ad219523c11c155b
Astrabassic <aseptraguna01@gmail.com>
2025-11-08 07:58:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+const scrollLeft = document.querySelector(".scroll-left");
+const scrollRight = document.querySelector(".scroll-right");
+const heroDiv = document.querySelector(".hero-img");
+const sectionContainer = document.querySelector("section");
+const bodyContainer = document.querySelector("body");
+const emblemDiv = document.querySelector(".emblem");
+const albumTitleSpan = document.querySelector(".album-title");
+const texts = document.querySelectorAll(".text");
+const albumNum = document.querySelector(".album-num");
+const spotifyWidget = document.querySelector(".spotify-widget iframe");
+const albums = [
+	{
+		album: "Sedia ASH",
+		emblem: "This is Bondan ",
+		"bg-color": ["#e9f54b", "#0D1827"],
+		"accent-color": "#white",
+		url: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/78/d2/b3/78d2b35f-8a06-9449-4d95-9416e16b206f/cover.jpg/600x600bf-60.jpg",
+		spotify:
+			"https://open.spotify.com/embed/track/43Ed54bKecnKeq5MWhb2BC?si=36c4106b20fd40e0"
+	},
+    {
+		album: "Alamak",
+		emblem: "This is Bondan ",
+		"bg-color": ["#3f6be4", "#0D1827"],
+		"accent-color": "#",
+		url:
+			"https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da840a19f951b7f97ed984fcd4f9",
+		spotify:
+			"https://open.spotify.com/embed/track/1vkwlZncsYqFukl2Q7M6Fe?si=04d3c6de50fc4ace"
+	},
+	
+];
+
+scrollLeft.addEventListener("click", () => handleClickScroll(-1));
+scrollRight.addEventListener("click", () => handleClickScroll(1));
+heroDiv.addEventListener("animationend", () => {
+	heroDiv.classList.remove("album-transition");
+	document.addEventListener("keydown", handleKeyScroll);
+	scrollLeft.disabled = false;
+	scrollRight.disabled = false;
+	scrollLeft.classList.remove("key-press-hover-left");
+	scrollRight.classList.remove("key-press-hover-right");
+
+	for (const text of texts) text.classList.add("show-texts");
+});
+
+const handleClickScroll = (val) => {
+	if (index + val >= 0 && index + val < albums.length) {
+		updateDisplay((index += val));
+	}
+};
+
+const handleKeyScroll = (e) => {
+	if (e.key == "ArrowLeft") {
+		scrollLeft.classList.add("key-press-hover-left");
+		handleClickScroll(-1);
+	}
+	if (e.key == "ArrowRight") {
+		scrollRight.classList.add("key-press-hover-right");
+		handleClickScroll(1);
+	}
+};
+let index = 0;
+
+const updateDisplay = (index) => {
+	let DELIMITER = "";
+
+	const album = albums[index];
+
+	for (const text of texts) text.classList.remove("show-texts");
+	emblemDiv.innerHTML = "";
+	scrollLeft.disabled = true;
+	scrollRight.disabled = true;
+	document.removeEventListener("keydown", handleKeyScroll);
+
+	sectionContainer.id = `hero-${album.album.toLowerCase().replace(" ", "-")}`;
+	bodyContainer.style.background = `linear-gradient(180deg, ${album["bg-color"][0]} 0%, ${album["bg-color"][1]} 100%)`;
+	heroDiv.style.backgroundImage = `url(${album.url})`;
+	albumTitleSpan.textContent = album.album;
+	spotifyWidget.src = album.spotify;
+
+	const number = index + 1;
+	albumNum.innerText = number >= 10 ? number + "." : `0${number}.`;
+	albumNum.style.color = album["accent-color"];
+
+	if (index === 3) scrollRight.classList.add("hide-arrow");
+	else scrollRight.classList.remove("hide-arrow");
+
+	createEmblem(album.emblem, DELIMITER[0] || undefined).forEach((node) =>
+		emblemDiv.append(node)
+	);
+
+	heroDiv.classList.add("album-transition");
+};
+
+const createEmblem = (string, delimiter = "•") => {
+	const spans = [];
+
+	string = string.trim().replaceAll(" ", delimiter) + delimiter;
+	const numChars = string.length;
+	const degVal = 90 / (numChars / 4);
+
+	string.split("").forEach((char, idx) => {
+		const span = document.createElement("span");
+		span.innerText = char;
+		span.style.transform = `rotate(${180 - degVal * idx}deg)`;
+		if (char === delimiter) span.style.color = albums[index]["accent-color"];
+		spans.push(span);
+	});
+
+	return spans;
+};
+
+updateDisplay(index);
